496b7451810fd0c92350136d5550869a9f2d2b35
DanRidh <danialridh1@gmail.com>
2023-04-09 02:17:13 +0900

    updated api call to only return movies

--- src/features/omdbApiSlice.ts
+++ src/features/omdbApiSlice.ts
@@ -33,7 +33,7 @@ const initialState: ApiState = {
 };
 
 export const fetchData = createAsyncThunk('omdbApi/fetchData', async (params: FetchDataParams) => {
-  const response: AxiosResponse<ApiData> = await axios.get(`${process.env.REACT_APP_OMDB_API_URL}?s=${params.inputQuery}?&apikey=${process.env.REACT_APP_OMDB_API_KEY}`);
+  const response: AxiosResponse<ApiData> = await axios.get(`${process.env.REACT_APP_OMDB_API_URL}?s=${params.inputQuery}&type=movie&apikey=${process.env.REACT_APP_OMDB_API_KEY}`);
   return response.data;
 });
 
